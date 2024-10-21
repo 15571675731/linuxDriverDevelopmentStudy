d8a17d0af7e3fc4e2fa1071c2c17e7c849b6f359
luoxl <lxl1596165548@gmail.com>
2024-10-21 11:33:29 +0800

    完善cdev的read和write功能 完善测试代码

--- 01_chardev/chardev.c
+++ 01_chardev/chardev.c
@@ -3,11 +3,24 @@
 #include <linux/fs.h> /* static struct file_operations 的头文件 */
 #include <linux/cdev.h>//cdev头文件
 #include <linux/uaccess.h>//处理用户空间和内核空间数据传递接口的头文件
+#include <linux/string.h>//额，linux不能使用普通的string.h,需要使用内核的string.h
+
+/////////////////////////////////////////////////
+//注意，使用此方法注册的驱动需要手动创建设备节点//
+//      sudo mknod /dev/chardev c 234 0        //
+/////////////////////////////////////////////////
+
 
 #define DEVICE_NAME			"chardev"
 
+#define BUFFER_SIZE			100
+
 static dev_t dev_num = 0;
 static struct cdev my_cdev;
+
+static char kernel_buf[] = "this is kernel buf!";
+static char write_buf[BUFFER_SIZE] = {0};
+static char read_buf[BUFFER_SIZE] = {0};
 
 int chardev_open(struct inode *inode, struct file *file)
 {
@@ -22,16 +35,52 @@ int chardev_release(struct inode *inode, struct file *file)
 	return 0;
 }
 
-ssize_t chardev_read(struct file *file, char __user *buf, size_t size, loff_t * offset)
+ssize_t chardev_read(struct file *file, char __user *buf, size_t len, loff_t * offset)
 {
+	int bytes_read;
 	printk("chardev_read\r\n");
+
+    if (*offset >= BUFFER_SIZE){
+		return 0;
+	}
+    
+    if (*offset + len > BUFFER_SIZE){
+		len = BUFFER_SIZE - *offset;
+	}
+	
+	// 拷贝内核数据到读缓冲区
+	memcpy(read_buf, kernel_buf, sizeof(kernel_buf));
+
+	// 将数据拷贝到用户buf
+	printk("r:of:%d, len:%d\r\n", (int)*offset, (int)len);
+    bytes_read = len - copy_to_user(buf, read_buf + *offset, len);
+    *offset += bytes_read;
+
+    printk(KERN_INFO "mychardev: Read %d bytes\n", bytes_read);
+    return bytes_read;
+
 	return 0;
 }
 
-ssize_t chardev_write(struct file *file, const char __user *buf, size_t size, loff_t * offset)
+ssize_t chardev_write(struct file *file, const char __user *buf, size_t len, loff_t * offset)
 {
-	printk("chardev_write\r\n");
-	return 0;
+	int bytes_written;
+	printk("你好，我只是个测试\r\n");
+    if (*offset >= BUFFER_SIZE){
+		return -ENOSPC;
+	}
+
+    if (*offset + len > BUFFER_SIZE){
+		len = BUFFER_SIZE - *offset;
+	}
+	
+	printk("offset:%d, len:%d\r\n", (int)*offset, (int)len);
+    bytes_written = len - copy_from_user(write_buf + *offset, buf, len);
+    *offset += bytes_written;
+
+    printk(KERN_INFO "mychardev: Wrote %d bytes\r\n", bytes_written);
+	printk("w:%s\r\n", write_buf);
+    return bytes_written;
 }
 
 
--- 01_chardev/testapp.c
+++ 01_chardev/testapp.c
@@ -1,24 +1,33 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <unistd.h>
 #include <fcntl.h>
 
-#define DEV_NAME        "/dev/chardev"
+
 
 int main()
 {
-    int fd = 0;
-    int buf = 0;
+    int fd = -1;
+    char read_buf[100] = {0};
+    char write_buf[100] = "hello world!";
 
-    fd = open(DEV_NAME, O_RDWR);
-    if(fd > 0)
-    {
-        write(fd, &buf, 1);
-        read(fd, &buf, 1);
-        close(fd);
+    fd = open("/dev/chardev", O_RDWR);
+    if(fd < 0){
+        perror("open cdev failed!");
+        return -1;
     }
+
+    write(fd, write_buf, strlen(write_buf));
+    close(fd);
+    fd = open("/dev/chardev", O_RDWR);
+    // lseek(fd, 0, SEEK_SET);
+    read(fd, read_buf, 100);
+    printf("read:%s\n", read_buf);
+    close(fd);
+    return 0;
 }
+
 
